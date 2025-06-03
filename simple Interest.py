cb6ab97fec108a348a989e46d785e1b404419cb2
Mohammed Aathif <vamohamedaathif@gmail.com>
2025-06-03 11:19:50 +0530

    Delete simple Interest.py

--- simple Interest.py
+++
@@ -1,7 +0,0 @@
-p = float(input("Enter Principal Amount : "))
-r = float(input("Enter Rate : "))
-t = int(input("Enter Time in Years : "))
-
-result = (p*r*t)/100
-
-print("Simple Interest is : " ,result)
